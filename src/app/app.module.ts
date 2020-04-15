d5dfd368dd29766737e4d5d6e1342220790e11c4
Jordan Deck <jordandeck060@gmail.com>
2020-04-14 19:24:05 -0500

    synching changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,39 +1,44 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { MatInputModule,
-        MatCardModule,
-        MatButtonModule,
-        MatToolbarModule,
-        MatExpansionModule,
-        MatDatepickerModule,
-        MatNativeDateModule
-} from '@angular/material';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import {
+  MatInputModule,
+  MatCardModule,
+  MatButtonModule,
+  MatToolbarModule,
+  MatExpansionModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+} from "@angular/material";
 
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderComponent } from './header/header.component';
-import { HomePageComponent } from './home-page/home-page.component';
-import { CreatePostComponent } from './posts/create-post/create-post.component';
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { HeaderComponent } from "./header/header.component";
+import { HomePageComponent } from "./home-page/home-page.component";
+import { CreatePostComponent } from "./posts/create-post/create-post.component";
+import { ListPostComponent } from "./posts/list-post/list-post.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomePageComponent,
-    CreatePostComponent
+    CreatePostComponent,
+    ListPostComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FormsModule,
     MatToolbarModule,
     MatButtonModule,
     MatInputModule,
     MatCardModule,
     MatExpansionModule,
     MatDatepickerModule,
-    MatNativeDateModule
+    MatNativeDateModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
