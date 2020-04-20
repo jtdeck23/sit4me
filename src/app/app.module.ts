65c98a45ff1de5787b02d470673b47d676e83f41
jtdeck23 <jordandeck060@gmail.com>
2020-04-19 21:05:29 -0500

    fixed some syntax issues

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import {
   MatInputModule,
   MatCardModule,
@@ -9,14 +9,14 @@ import {
   MatExpansionModule,
   MatDatepickerModule,
   MatNativeDateModule,
-} from "@angular/material";
+} from '@angular/material';
 
-import { AppComponent } from "./app.component";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { HeaderComponent } from "./header/header.component";
-import { HomePageComponent } from "./home-page/home-page.component";
-import { CreatePostComponent } from "./posts/create-post/create-post.component";
-import { ListPostComponent } from "./posts/list-post/list-post.component";
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { CreatePostComponent } from './posts/create-post/create-post.component';
+import { ListPostComponent } from './posts/list-post/list-post.component';
 
 @NgModule({
   declarations: [
